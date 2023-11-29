173c83e175810e4feaaefbe58e811ee131e6b7b2
jmechristian <jmechristian@gmail.com>
2023-11-29 13:19:41 -0500

    pack-admin-2

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,100 @@
-import Image from 'next/image'
+'use client';
+import { useLayoutEffect, useRef } from 'react';
+import Image from 'next/image';
+import { gsap } from 'gsap';
+import TextPlugin from 'gsap/TextPlugin';
+import { Press_Start_2P } from 'next/font/google';
+
+gsap.registerPlugin(TextPlugin);
+const pressStart = Press_Start_2P({ weight: '400', subsets: ['latin'] });
 
 export default function Home() {
+  const sceneRef = useRef(null);
+
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      gsap.to('.back', { opacity: 1, duration: 1 });
+      gsap.to('.back', { scale: 1, duration: 1.5 });
+      gsap.to('.computer-backdrop', {
+        opacity: 1,
+        delay: 0.75,
+        duration: 0.75,
+      });
+      gsap.to('.computer', {
+        bottom: 0,
+        scale: 1.25,
+        duration: 1,
+        ease: 'power4.easeInOut',
+      });
+      gsap.to('.text', {
+        duration: 0.5,
+        text: '***PACK ADMIN***',
+        ease: 'none',
+        delay: 1.25,
+      });
+      gsap.to('.text-1', { opacity: 1, duration: 0.05, delay: 2 });
+      gsap.to('.text-2', { opacity: 1, duration: 0.05, delay: 2.25 });
+      gsap.to('.text-3', { opacity: 1, duration: 0.05, delay: 2.5 });
+      gsap.to('.text-4', {
+        duration: 0.5,
+        text: '*****************',
+        ease: 'none',
+        delay: 2.75,
+      });
+    }, sceneRef);
+
+    return () => ctx.revert();
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main
+      className='flex min-h-screen min-w-screen w-full relative bg-slate-900 overflow-hidden'
+      ref={sceneRef}
+    >
+      <div
+        className='w-full h-full bg-slate-700 opacity-0 absolute z-10 inset-0 bg-cover bg-no-repeat bg-center back scale-125'
+        style={{
+          backgroundImage: `url('https://packschool.s3.amazonaws.com/admin-back-main.png')`,
+        }}
+      ></div>
+      <div className='opacity-0 w-full h-full bg-gradient-to-t from-black/90 fixed inset-0 z-[15] computer-backdrop'></div>
+      <div className='w-full mx-auto absolute -bottom-full z-20 computer scale-90'>
+        <div className='w-full max-w-[50%] mx-auto relative'>
+          <Image
+            src={'https://packschool.s3.amazonaws.com/pack-admin-computer.png'}
+            alt='pack admin computer'
+            width={1120}
+            height={957}
+          />
+          <div className='w-1/2 h-40 absolute z-30 top-20 -translate-x-1/2 left-1/2'>
+            <div className='flex flex-col gap-9 pl-3'>
+              <div
+                className={`${pressStart.className} text-xl text-green-500 text`}
+              ></div>
+              <div className='flex flex-col'>
+                <div
+                  className={`${pressStart.className} text-lg text-green-500 text-1 opacity-0`}
+                >
+                  &gt;CLICK HERE
+                </div>
+                <div
+                  className={`${pressStart.className} text-lg text-green-500 text-2 opacity-0`}
+                >
+                  &gt;TO ACCESS
+                </div>
+                <div
+                  className={`${pressStart.className} text-lg text-green-500 text-3 opacity-0`}
+                >
+                  &gt;PSCHOOL DATA
+                </div>
+              </div>
+              <div
+                className={`${pressStart.className} text-lg text-green-500 text-4`}
+              ></div>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [{ hostname: 'packschool.s3.amazonaws.com' }],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
